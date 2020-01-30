3ac195752196556e8f94ad3d94e5f8c043526728
Justin <43622681+vuongjn@users.noreply.github.com>
2020-01-30 16:57:15 -0700

    Update Zoo.java
    initial document push

---
+++ Project1/Zoo.java
@@ -0,0 +1,42 @@
+public class Animal
+{
+  // Constructor of Animal Class
+  public Animal()
+  {
+  }
+
+  // Sleep Method
+
+  public void sleep()
+  {
+
+  }
+}
+
+public class Feline extends Animal
+{
+
+}
+
+public class Canine extends Animal
+{
+
+}
+
+public class Pachyderm extends Animal
+{
+
+}
+public class Zookeeper
+{
+  // Construtor for Zookeeper class
+  public Zookeeper()
+  {
+  }
+
+  //Wake up animals
+  public void wakeAnimals()
+  {
+
+  }
+}
