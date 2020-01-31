4a701a516d9cfe93742265b124c5e7d80b56b9dd
Sabrina Touch <sabrinatouch@Sabrinas-MBP.local.tld>
2020-01-30 17:10:03 -0700

    Added comments for readability

--- Project1/Zoo.java
+++ Project1/Zoo.java
@@ -1,10 +1,14 @@
-// Team Members: Justin Vuong, Sabrina Touch, Andy Kim
+/**
+  Team Members: Justin Vuong, Sabrina Touch, Andy Kim
+*/
 
+/* -------------------------- Animal -------------------------- */
 public class Animal
 {
   // Constructor of Animal Class
   public Animal()
   {
+
   }
 
   // Sleep Method
@@ -12,75 +16,94 @@ public class Animal
   {
 
   }
+
+  // Eat Method
+  public void eat()
+  {
+
+  }
+
+  // Wake Up Method
+  public void wakeUp()
+  {
+
+  }
 }
 
+/* -------------------------- Level 1.2 -------------------------- */
 public class Feline extends Animal
 {
   // Roam Method
   public void roam()
   {
-    
+
   }
 }
 
+/* -------------------------- Level 1.3 -------------------------- */
 public class Cat extends Feline
 {
-  
+
 }
 
 public class Tiger extends Feline
 {
-  
+
 }
 
 public class Lion extends Feline
 {
-  
+
 }
 
+/* -------------------------- Level 2.2 -------------------------- */
 public class Canine extends Animal
 {
   // Roam Method
   public void roam()
   {
-    
+
   }
 }
 
+/* -------------------------- Level 2.3 -------------------------- */
 public class Wolf extends Canine
 {
-  
+
 }
 
 public class Dog extends Canine
 {
-  
+
 }
 
+/* -------------------------- Level 3.2 -------------------------- */
 public class Pachyderm extends Animal
 {
   // Roam Method
   public void roam()
   {
-    
+
   }
 }
 
+/* -------------------------- Level 3.3 -------------------------- */
 public class Hippo extends Pachyderm
 {
-  
+
 }
 
 public class Elephant extends Pachyderm
 {
-  
+
 }
 
 public class Rhino extends Pachyderm
 {
-  
+
 }
 
+/* -------------------------- Zookeeper -------------------------- */
 public class Zookeeper
 {
   // Construtor for Zookeeper class
@@ -95,7 +118,8 @@ public class Zookeeper
   }
 }
 
+/* -------------------------- Main -------------------------- */
 public static void main(String[] args)
 {
-  
+
 }
