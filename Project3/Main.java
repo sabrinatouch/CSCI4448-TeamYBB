a76e7e42d38cb5301ef7c8de384ba458dbb68ee0
sabrinatouchCU <sabrina.touch@colorado.edu>
2020-02-29 08:23:13 -0700

    Created Main.java

---
+++ Project3/Main.java
@@ -0,0 +1,19 @@
+/********************************************************
+**                                                     **
+** Team Members: Justin Vuong, Sabrina Touch, Andy Kim **
+**        CSCI4448-Object Oriented Programming         **
+**                     Project 3                       **
+**                                                     **
+********************************************************/
+
+import java.util.*;
+import java.io.*;
+
+/* -------------------------- Main -------------------------- */
+public class Main
+{
+  public static void main(String[] args)
+  {
+    //
+  }
+}
